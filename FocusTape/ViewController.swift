0253a0c8efb5f39b92f5361dd34648b18bfb2dc9
Artem Pechenkin <artem.s.pechenkin@gmail.com>
2020-09-17 22:54:13 -0700

    Initial Commit

---
+++ FocusTape/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FocusTape
+//
+//  Created by Artem Pechenkin on 9/17/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
